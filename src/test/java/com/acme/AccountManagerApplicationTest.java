e7117feb91a601ab63db3d4d4610275e723cf339
unknown <sisa.anand@gmail.com>
2022-07-03 03:26:52 +0800

    change to extendwith annotation

--- src/test/java/com/acme/AccountManagerApplicationTest.java
+++ src/test/java/com/acme/AccountManagerApplicationTest.java
@@ -13,11 +13,15 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.acme.dto.Account;
@@ -27,7 +31,7 @@ import com.acme.repository.AccountDetailRepository;
 import com.acme.result.Result;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
 public class AccountManagerApplicationTest {
